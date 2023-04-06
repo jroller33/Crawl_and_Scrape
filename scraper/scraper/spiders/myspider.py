429d6c44f0c158d9660f6d84b20ed16c8842ac8e
John Roller <jtroller33@gmail.com>
2023-04-06 13:42:27 -0500

    EcomSpider class

--- scraper/scraper/spiders/myspider.py
+++ scraper/scraper/spiders/myspider.py
@@ -8,3 +8,18 @@ class MyspiderSpider(scrapy.Spider):
 
     def parse(self, response):
         pass
+
+from scraper.items import Product
+
+class EcomSpider(scrapy.Spider):
+    name = 'ecom_spider'
+    allowed_domains = ['clever-lichterman-044f16.netlify.app']
+    start_urls = ['https://clever-lichterman-044f16.netlify.app/products/taba-cream.1/']
+
+    def parse(self, response):
+        item = Product()
+        item['product_url'] = response.url
+        item['price'] = response.xpath("//div[@class='my-4']/span/text()").get()
+        item['title'] = response.xpath('//section[1]//h2/text()').get()
+        item['img_url'] = response.xpath("//div[@class='product-slider']//img/@src").get(0)
+        return item
