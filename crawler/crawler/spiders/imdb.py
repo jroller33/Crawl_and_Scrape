f7401e5871b78d0c9b29f285899714a8df8c3fcd
John Roller <jtroller33@gmail.com>
2023-04-09 17:24:47 -0500

    removed old crawler

--- crawler/crawler/spiders/imdb.py
+++
@@ -1,8 +0,0 @@
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-
-class ImdbCrawler(CrawlSpider):
-    name = 'imdb'
-    allowed_domains = ['www.imdb.com']
-    start_urls = ['https://www.imdb.com/']
-    rules = (Rule(LinkExtractor()),)
