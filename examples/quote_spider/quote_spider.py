84fc9a8ccef8f8a03b2d2c4f0e0e92697014b2c3
John Roller <jtroller33@gmail.com>
2023-04-09 17:55:37 -0500

    added comments

--- examples/quote_spider/quote_spider.py
+++ examples/quote_spider/quote_spider.py
@@ -1,6 +1,7 @@
 # https://docs.scrapy.org/en/latest/intro/overview.html
 
 # to run this spider, open terminal INSIDE the `quote_spider/` folder, then run `scrapy runspider quotes_spider.py -o quotes.jsonl`
+# `quotes.jsonl` in this folder is the log generated from running this spider.
 
 import scrapy
 
