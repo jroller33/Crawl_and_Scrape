67161db0f9657388c20f8c82ea321fbed0c72993
John Roller <jtroller33@gmail.com>
2023-04-09 17:47:00 -0500

    added comment on how to run spider

--- examples/quote_spider/quote_spider.py
+++ examples/quote_spider/quote_spider.py
@@ -1,5 +1,7 @@
 # https://docs.scrapy.org/en/latest/intro/overview.html
 
+# to run this spider, open terminal INSIDE the `quote_spider/` folder, then run `scrapy runspider quotes_spider.py -o quotes.jsonl`
+
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
